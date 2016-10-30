75843cba179f247c06f36d405e41558d58d06c33
dursunkoc <dursunKoc@gmail.com>
2016-10-30 22:57:10 +0300

    Started Project with a Sample Kafka Producer App.

---
+++ src/main/java/com/aric/samples/ProducerAppSample.java
@@ -0,0 +1,31 @@
+package com.aric.samples;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+/**
+ * Hello world!
+ *
+ */
+public class ProducerAppSample {
+    public static void main(final String[] args) {
+        final Properties prop = new Properties();
+        prop.put("bootstrap.servers", "localhost:9093, localhost:9092");
+        prop.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        prop.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        prop.put("batch.size", "10000");
+        prop.put("max.block.ms", "10000");
+        prop.put("linger.ms", "10000");
+
+        try (final KafkaProducer<String, String> myProducer = new KafkaProducer<>(prop)) {
+            for (int i = 0; i < 150; i++) {
+                final ProducerRecord<String, String> record = new ProducerRecord<String, String>("benim_basiligim",
+                        Integer.toString(i), "My Message " + i);
+                myProducer.send(record);
+            }
+        }
+
+    }
+}
